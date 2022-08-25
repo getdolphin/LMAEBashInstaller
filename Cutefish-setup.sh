fcdb8d2edc389eac9dfd8f36929ffba3ad38390b
Nex389 <72895529+Nex320@users.noreply.github.com>
2022-08-25 17:54:30 -0400

    Update Cutefish-setup.sh
    Stops yay from being an idiot and bricking the tty

--- Cutefish-setup.sh
+++ Cutefish-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # LMAE Bash Installer | Cutefish Editon Setup
 if (whiptail --title "Linux Mint Arch Edition | Cutefish Setup" --yesno "Are you sure you want to continue installing?" 8 40 --yes-button "Yes" --no-button "No"); then
-	yay -Syyu cutefish vlc cutefish-calculator cutefish-core cutefish-cursor-themes-git cutefish-gtk-themes-git cutefish-kwin-plugins cutefish-screenshot-git cutefish-videoplayer-git sddm | \
+	yay -Syyu --noconfirm cutefish vlc cutefish-calculator cutefish-core cutefish-cursor-themes-git cutefish-gtk-themes-git cutefish-kwin-plugins cutefish-screenshot-git cutefish-videoplayer-git sddm | \
     	tr '[:upper:]' '[:lower:]' | whiptail --title "Linux Mint Arch Edition | Installing Packages" \
         --gauge "The installer is now doing required steps to provide you a great desktop..." 7 70 0
 	systemctl enable sddm
